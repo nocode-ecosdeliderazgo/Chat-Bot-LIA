c90c7f15c2df07ea09e12689583d0318388fe46c
Draax17 <nocode@ecosdeliderazgo.com>
2025-09-02 17:53:28 -0600

    volver a poner el chat online de antes

--- src/scripts/cursos.js
+++ src/scripts/cursos.js
@@ -32,7 +32,7 @@ function renderCards(list, grouped=false){
         <div class="instructor">${c.instructor}</div>
         <div class="meta"><span>⭐ ${c.rating.toFixed(1)}</span><span class="price">${formatPrice(c.price)}</span></div>
         <div class="cta">
-                     <button class="enroll acquired" onclick="location.href='chat-online-clean.html'">Adquirido</button>
+                     <button class="enroll acquired" onclick="location.href='Chat-Online/chat-online.html'">Adquirido</button>
           <button class="wishlist"><i class='bx bx-heart'></i></button>
         </div>
       </div>
